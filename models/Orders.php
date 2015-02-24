3ee0d7bd450e3df50a80ee581fc6aeb2a0aeb287
resh <reshevskiy@mail.ru>
2015-02-24 17:42:53 +0300

    1

--- models/Orders.php
+++ models/Orders.php
@@ -43,20 +43,21 @@ class Orders extends \yii\base\Model
      */
     public function rules()
     {
-        return [
+        return [            
             [['sum', 'exp_month', 'exp_year', 'cvv', 'name', 'last_name', 'email', 'city', 'phone', 'card_no', 'state', 'address', 'zip_code'], 'required'],
-//            [['sum'], 'number'],
             [['sum'], 'number', 'min' => 0.01],
-            [['exp_month', 'exp_year', 'cvv'], 'integer'],
+            [['exp_month', 'exp_year', 'cvv', 'card_no', 'zip_code'], 'integer'],
             [['name', 'last_name', 'email', 'city'], 'string', 'max' => 64],
             [['email'], 'email'],
-            [['card_no'], 'string', 'max' => 16],
-            [['phone'], 'string', 'min' => 16, 'max' => 16],
+            [['card_no'], 'string', 'max' => 16, 'min' => 16],
+            [['phone'], 'string', 'max' => 16],
             [['state'], 'string', 'min' => 2, 'max' => 2],
             [['address'], 'string', 'max' => 255],
-            [['zip_code'], 'string', 'max' => 5],
+            [['zip_code'], 'string', 'max' => 5, 'min' => 5],
+            [['cvv'], 'string', 'max' => 3, 'min' => 3],
             ['hash', 'checkHash'],
             ['exp_year', 'checkDate', 'params' =>['month' => 'exp_month']],
+            
         ];
     }
 
